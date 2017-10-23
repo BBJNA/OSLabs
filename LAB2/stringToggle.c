6584a53927b851eb9a8610ffb9c587e6e1ca67a1
JordanWilliams93 <j.e.smailliw@gmail.com>
2017-10-22 20:16:33 -0400

    Pipes Done

--- LAB2/stringToggle.c
+++ LAB2/stringToggle.c
@@ -4,134 +4,133 @@
 #include <sys/wait.h>
 #include <string.h>
 #include <ctype.h>
-#include <stdbool.h>
-#include <errno.h>
+
+
+#define P1_READ     0
+#define P2_WRITE    1
+#define P2_READ     2
+#define P1_WRITE    3
 
 int StringValid(char *string);
 char *ToggleString(char string[]);
 
-int main(int argc, char *argv[]){
-
-	char ParentString[100], ChildString[100];
-	int length, i;
+int main(int argc, char *argv[])
+{
+    int fd[4], length;
+    pid_t pid;
+    char ParentString[100], ChildString[100];
 
 	printf("Enter in a string: ");
 	scanf("%s", ParentString);
 	
-	char test [strlen(ParentString)];
-	strcpy(test, ToggleString(ParentString));
-
-	printf("%s\n", test);
-
 	int valid = StringValid(ParentString);
 
  	if(valid != 0){
 	
-	return -1;
+    	exit(EXIT_FAILURE);
 	
 	}
 
-	int fd [4];
-	pid_t pid;
+        if (pipe(fd) < 0)
+        {
+            perror("Failed to allocate pipes");
+            exit(EXIT_FAILURE);
+        }
 
-	for(i=0;i<2;i++){
+          if (pipe(fd+2) < 0)
+        {
+            perror("Failed to allocate pipes");
+            exit(EXIT_FAILURE);
+        }
 
-		if(pipe(fd+(i*2))<0){
-			
-			perror("Pipe wasn't created");
-			exit(EXIT_FAILURE);			
-			
-		}
-	}
+    // fork() returns 0 for child process, child-pid for parent process.
+    if ((pid = fork()) < 0)
+    {
+        perror("Failed to fork process");
+        return EXIT_FAILURE;
+    }
 
-	if((pid = fork())==-1){
+    // if the pid is zero, this is the child process
+    if (pid == 0)
+    {
+        // Child. Start by closing descriptors we
+        //  don't need in this process
+        close(fd[P1_READ]);
+        close(fd[P1_WRITE]);
 
-		perror("fork");
-		return 1;	
+        // used for output
+        pid = getpid();
 
-	}else{
-	
-		if(pid == 0){
+        // wait for parent to send us a value
+        length = read(fd[P2_READ], &ChildString, sizeof(ChildString));
+        if (length < 0)
+        {
+            perror("Child: Failed to read data from pipe");
+            exit(EXIT_FAILURE);
+        }
+        else if (length == 0)
+        {
+            // not an error, but certainly unexpected
+            fprintf(stderr, "Child: Read EOF from pipe");
+        }
+        else
+  		{
+  			strcpy(ChildString,ToggleString(ChildString));
+            if (write(fd[P2_WRITE], &ChildString, sizeof(ChildString)) < 0)
+            {
+                perror("Child: Failed to write response value");
+                exit(EXIT_FAILURE);
+            }
+        }
 
-			close(fd[0]);
-			close(fd[3]);
-			
-			pid = getpid();
+        // finished. close remaining descriptors.
+        close(fd[P2_READ]);
+        close(fd[P2_WRITE]);
 
-			length = read(fd[2], ChildString, strlen(ChildString));
+        return EXIT_SUCCESS;
+    }
 
-			if(length < 0){
+    // Parent. close unneeded descriptors
+    close(fd[P2_READ]);
+    close(fd[P2_WRITE]);
 
-				perror("Pipe transfer failed");
-				exit(EXIT_FAILURE);	
+    // used for output
+    pid = getpid();
 
-			}
-			
-			if(length == 0){
+    // send a value to the child
+    if (write(fd[P1_WRITE], &ParentString, sizeof(ParentString)) != sizeof(ParentString))
+    {
+        perror("Parent: Failed to send value to child ");
+        exit(EXIT_FAILURE);
+    }
 
-				perror("Something went wrong");
+    // now wait for a response
+    length = read(fd[P1_READ], &ParentString, sizeof(ParentString));
+    if (length < 0)
+    {
+        perror("Parent: failed to read value from pipe");
+        exit(EXIT_FAILURE);
+    }
+    else if (length == 0)
+    {
+        // not an error, but certainly unexpected
+        fprintf(stderr, "Parent(%d): Read EOF from pipe", pid);
+    }
+    else
+    {
+        // report what we received
+        printf("Answer: %s\n", ParentString);
+    }
 
+    // close down remaining descriptors
+    close(fd[P1_READ]);
+    close(fd[P1_WRITE]);
 
-			}else{
+    // wait for child termination
+    wait(NULL);
 
-				printf("\nChild got string\n");
-				if(write(fd[1], ToggleString(ChildString), strlen(ChildString))<0){
-				
-					perror("Child did not write");
-					exit(EXIT_FAILURE);	
-		
-				}
-			
-			}
-
-			close(fd[2]);
-			close(fd[1]);
-			return (EXIT_SUCCESS);
-			
-
-		}else{
-
-			close(fd[2]);
-			close(fd[1]);
-
-			pid = getpid();
-
-			if(write(fd[3], ParentString, (strlen(ParentString))) != strlen(ParentString)){
-			
-				perror("Parent did not write properly");
-				exit(EXIT_FAILURE);	
-
-			}
-
-			length = read(fd[0], ParentString, strlen(ParentString));
-			printf("%d\n", length);
-
-			if(length < 0){
-
-					perror("Pipe transfer failed 2");
-					exit(EXIT_FAILURE);			
-
-				}
-				
-				if(length == 0){
-
-					perror("Something went wrong 2");
-
-				}else{
-
-					printf("%s",ParentString);
-				
-				}
-
-				close(fd[0]);
-				close(fd[3]);
-				wait(NULL);
-				return (EXIT_SUCCESS);
-
-		}
-	}
-
-}	
+    return EXIT_SUCCESS;
+}
 
 int StringValid(char *string){
 
@@ -150,7 +149,6 @@ int StringValid(char *string){
 char *ToggleString(char str[]){
 
 	int i = 0;
-	printf("%s\n",str);
 
 	while(i<strlen(str)){
 		
