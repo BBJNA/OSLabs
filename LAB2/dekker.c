85022e91b10cccd9727ca78a832464f1ae771866
JordanWilliams93 <j.e.smailliw@gmail.com>
2017-11-04 21:06:39 -0400

    Change it up

--- LAB2/dekker.c
+++ LAB2/dekker.c
@@ -1,48 +1,99 @@
-//INCLUDES
-#include <fcntl.h>
+#include <pthread.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <errno.h>
-#include <sys/types.h>
-#include <sys/wait.h>
+#include <string.h>
 #include <unistd.h>
 
+int flag[2], i = 0, j = 1, turn;
+
+void p1();
+void p2();
 
 //Parent Process
 int main(int argc, char *argv[]){
 
-int turn = 1;
-int i = 0, j = 1, k;
-int flag[] = {1, 1};
+	p1();
+	p2();
 
-do{
-
-	
-	printf("Entry Section initializing");
-	
-	flag[i] = 1;
-	turn = j;
-
-	for(k = 0; k<5; k++){
-
-		printf(".");
-		sleep(1);
-
-	}
-	printf("Setup complete!\n");
-
-	while(flag[j] && turn == j){
-
-
-	}
-
-	flag[i] = 0;
-
-	//remainder section
-
-}while(1);
-
-return (EXIT_SUCCESS);
+	return 0;
 
 }
 
+void p1 (){
+
+	char string[] = "P1";
+
+	do
+	{
+		//Entry Section
+		printf("%s is in the Entry Section\n", string);
+		sleep(1);	
+		flag[i] = 1;
+
+		while(flag[j]){
+			if (turn == j)
+			{
+				flag[i] = 0;
+				while(turn == j);
+				flag[i] = 1; 			
+			}	
+		}
+
+		//Critical Section
+		printf("%s is in the Critical Section. Please wait.....\n", string);
+		sleep(5);	
+
+		//Exit Section
+		printf("%s is in the Exit Section\n", string);
+		sleep(1);	
+
+		turn = j;
+		flag[i] = 0;
+
+		//Remainder Section
+		printf("%s is in the Remainder Section\n", string);
+		sleep(1);	
+		break;
+
+	} while (1);
+
+}
+
+void p2 (){
+
+	char string[] = "P2";
+
+	do
+	{
+		//Entry Section
+		printf("%s is in the Entry Section\n", string);
+		sleep(1);	
+		flag[j] = 1;
+
+		while(flag[i]){
+			if (turn == i)
+			{
+				flag[j] = 0;
+				while(turn == i);
+				flag[j] = 1; 			
+			}	
+		}
+
+		//Critical Section
+		printf("%s is in the Critical Section. Please wait.....\n", string);
+		sleep(5);
+
+		//Exit Section
+		printf("%s is in the Exit Section\n", string);
+		sleep(1);	
+
+		turn = i;
+		flag[j] = 0;
+
+		//Remainder Section
+		printf("%s is in the Remainder Section\n", string);
+		sleep(1);	
+
+		break;
+	} while (1);
+
+}
